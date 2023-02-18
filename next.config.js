7e254faf9b95a474a5e26814ffb1897b6574b91a
KirillGoryakin <kirillgoryakin.dev@gmail.com>
2023-02-18 17:53:18 +0500

    Add ability to disable image optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    unoptimized: process.env.NEXT_PUBLIC_UNOPTIMIZED === 'true' ? true : false,
+  },
 };
 
 module.exports = nextConfig;
